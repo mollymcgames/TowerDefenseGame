74c48fcc586a6a9d13b6b3842503aa8ebccea16b
Molly <mollymcconaghy@gmail.com>
2023-11-15 11:35:26 +0000

    Two types of towers

--- Assets/RedMoonTower.cs
+++ Assets/RedMoonTower.cs
@@ -1,33 +1,109 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class RedMoonTower : MonoBehaviour
 {
+    public GameObject energyBallPrefab; // Add the energy ball prefab here
+    public Transform firePoint; // An empty game object as the fire point to assign in the inspector
+    public Transform targetWaypoint; // The target waypoint the enemy moves towards
 
-    private Animator myAnimator; //The animator component
+    [SerializeField] private float fireRate = 1.0f; // The rate of fire
+    [SerializeField] public float energyBallSpeed = 2f; // The speed of the arrow
+    [SerializeField] private float firingRange = 5f; // The desired firing range
+
+    private float timeUntilFire; // Timer to keep track of when to fire
+    public List<GameObject> activeEnergyBalls = new List<GameObject>(); // List to hold active arrows
+    private bool shouldFire = true; // A flag to determine if the tower should fire
+
     // Start is called before the first frame update
     void Start()
     {
-        //GEt the animator component
-        myAnimator = GetComponent<Animator>();
-
-        //check if the Animator component exists 
-        if (myAnimator != null)
-        {
-            //trigger the animation
-            myAnimator.SetTrigger("Idle");
-        }
-        else
-        {
-            Debug.LogError("Animator component not found");
-        }
-        
+        timeUntilFire = 0f; // Set the timer to the fire rate
     }
 
     // Update is called once per frame
-    void Update()
+    protected virtual void Update()
     {
-        
+        // Countdown the timer until it's time to fire the next arrow
+        timeUntilFire -= Time.deltaTime;
+
+        // If the timer reaches zero or below, fire an arrow and reset the timer
+        if (timeUntilFire <= 0 && shouldFire)
+        {
+            FireEnergyBall();
+            timeUntilFire = fireRate;
+        }
+
+        // Create a separate list to hold arrows that are no longer active
+        List<GameObject> inactiveEnergyBalls = new List<GameObject>();
+
+        // If an arrow is present, move it towards the enemy's position
+        foreach (GameObject energyBall in activeEnergyBalls)
+        {
+            if (energyBall != null)
+            {
+                EnergyBallScript energyBallScript = energyBall.GetComponent<EnergyBallScript>();
+                if (energyBallScript != null)
+                {
+                    energyBallScript.EnergyBallBehaviour(targetWaypoint, shouldFire, inactiveEnergyBalls, energyBallSpeed);
+                }
+            }
+        }
+
+        // Remove the inactive arrows from the active arrows list
+        foreach (GameObject energyBall in inactiveEnergyBalls)
+        {
+            inactiveEnergyBalls.Remove(energyBall);
+        }
+    }
+
+    private void FireEnergyBall()
+    {
+        // The Tower should find the closest enemy before firing an arrow. If there are multiple enemies on the scene it will prioritize the nearest one.
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy"); // Get all the enemies on the scene
+        GameObject closestEnemy = null; // The closest enemy
+        float closestDistance = Mathf.Infinity; // The distance to the closest enemy
+        foreach (GameObject enemy in enemies)
+        {
+            if (enemy != null)
+            {
+                float distance = Vector3.Distance(transform.position, enemy.transform.position); // Calculate the distance between the tower and the enemy
+                //we will also use this for the ifcheck 
+                if (distance < closestDistance && distance <= firingRange) // If the distance is less than the closest distance
+                {
+                    closestDistance = distance; // Set the closest distance to the distance
+                    closestEnemy = enemy; // Set the closest enemy to the enemy
+                }
+            }
+        }
+        if (closestEnemy != null)
+        {
+            NavMeshAgent enemyAgent = closestEnemy.GetComponent<NavMeshAgent>();
+            ShootEnergyBall(enemyAgent);
+        }
+    }
+
+
+    protected virtual void ShootEnergyBall(NavMeshAgent enemyAgent)
+    {
+            // Instantiate an arrow prefab at the fire point position and rotation
+            GameObject energyBall = Instantiate(energyBallPrefab, firePoint.position, firePoint.rotation);
+
+            // Calculate the direction towards the enemy agent
+            Vector3 direction = (enemyAgent.transform.position - firePoint.position).normalized;
+
+            energyBall.transform.rotation = Quaternion.LookRotation(Vector3.forward, direction) * Quaternion.Euler(0, 0, 270);
+
+            // Get the arrow component
+            Rigidbody2D rb = energyBall.GetComponent<Rigidbody2D>();
+
+            // Set the initial velocity of the arrow towards the enemy
+            if (rb != null)
+            {
+                rb.velocity = direction * energyBallSpeed;
+                activeEnergyBalls.Add(energyBall); // Add the arrow to the list of active arrows
+            }
     }
 }
---
+++ Assets/Scripts/EnergyBallScript.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyBallScript : MonoBehaviour
+{
+   public int damageAmount = 1; // The damage the energyball will do to the enemy
+
+    private Rigidbody2D energyBallRigidbody; // The energy ball rigidbody component
+
+    public float range = 1.0f; // The range of the energy ball. Will be time based. Was 2 before
+    private float timer = 0.0f; // The timer for the energy ball which will countdown the range
+    private Vector3 initialDirection; // The initial direction of the energy ball
+
+
+    private void Start()
+    {
+        energyBallRigidbody = GetComponent<Rigidbody2D>(); // Get the energy ball rigidbody component
+        timer = range; // Set the timer to the range
+
+    }
+
+    void FixedUpdate() 
+    {
+        timer -= Time.deltaTime; // Decrement the timer
+        if(timer <= 0.0f) // If the timer is less than or equal to 0
+        {
+            Destroy(gameObject); // Destroy the arrow
+        }
+    }
+
+
+    public void EnergyBallBehaviour(Transform targetWaypoint, bool shouldFire, List<GameObject> inactiveEnergyBalls, float energyBallSpeed)
+    {
+        //Similar logic to the TowerArcher.cs script of finding the closest enemy and dealing damage to it
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy"); // Get all the enemies on the scene
+        GameObject closestEnemy = null; // The closest enemy
+        float closestDistance = Mathf.Infinity; // The distance to the closest enemy
+        foreach (GameObject enemy in enemies)
+        {
+            if (enemy != null)
+            {
+                float distance = Vector3.Distance(transform.position, enemy.transform.position); // Calculate the distance between the tower and the enemy
+                if (distance < closestDistance) // If the distance is less than the closest distance
+                {
+                    closestDistance = distance; // Set the closest distance to the distance
+                    closestEnemy = enemy; // Set the closest enemy to the enemy
+                }
+            }
+        }
+        if(closestEnemy != null)
+        {
+            UnityEngine.AI.NavMeshAgent enemyAgent = closestEnemy.GetComponent<UnityEngine.AI.NavMeshAgent>();
+            if(enemyAgent != null && targetWaypoint != null)
+            {
+                if(Vector3.Distance(enemyAgent.transform.position, targetWaypoint.position) <= 1.2f)
+                {
+                    // Stop firing when the enemy reaches the waypoint
+                    shouldFire = false;
+                    inactiveEnergyBalls.Add(gameObject);
+                    Destroy(gameObject);
+                }
+                else if(Vector3.Distance(transform.position, enemyAgent.transform.position) <= 0.5f)
+                {
+                    // Handle the arrow hitting the enemy
+                    DealDamage(closestEnemy);
+                    inactiveEnergyBalls.Add(gameObject);
+                    Destroy(gameObject);
+                }
+                else if(shouldFire)
+                {
+                    transform.position += initialDirection * energyBallSpeed * Time.deltaTime;
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            DealDamage(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+
+    public void DealDamage(GameObject enemy)
+    {
+        // GameObject enemy = GameObject.FindGameObjectWithTag("Enemy");
+        if(enemy != null)
+        {
+            EnemyHealthZombie enemyHealthManager = enemy.GetComponent<EnemyHealthZombie>();
+            if(enemyHealthManager != null)
+            {
+                enemyHealthManager.TakeDamage(damageAmount); // Change the value if needed
+            }
+
+            //@TODO: Remove this later as the health managers will merge
+            EnemyHealthGoblinRider enemyHealthBat = enemy.GetComponent<EnemyHealthGoblinRider>();
+            if(enemyHealthBat != null)
+            {
+                enemyHealthBat.TakeDamage(damageAmount); // Change the value if needed
+            }
+        }
+    }
+}
