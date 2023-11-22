53b239a219122a9c5391faf4621bb3ec8ea78a13
Molly <mollymcconaghy@gmail.com>
2023-11-22 16:06:23 +0000

    Add boss

---
+++ Assets/AncientSkeletonController.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AncientSkeletonController : EnemyController
+{
+    //This class inherits from enemy controller for now, but will have its own methods and attributes in the future
+}
