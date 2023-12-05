b5a6c259815aec7751a9530297bffdb4c8b091b8
Molly <mollymcconaghy@gmail.com>
2023-12-05 17:12:20 +0000

    knightIdle

---
+++ Assets/KnightController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightController : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+
+    private Animator anim;
+    [SerializeField] private float speed; //The speed of the knight
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); //Get the rigidbody component 
+        anim = GetComponent<Animator>(); //Get the animator component
+    }
+
+    void Update()
+    {
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * speed * Time.deltaTime; //Get the input from the keyboard and move the knight
+
+        anim.SetFloat("moveX", rb.velocity.x); //Set the moveX parameter in the animator
+        anim.SetFloat("moveY", rb.velocity.y); //Set the moveY parameter in the animator
+    }
+}
